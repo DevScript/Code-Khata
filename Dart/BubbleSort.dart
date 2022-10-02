fdaee43cdf4d700f0feef5ef180b26e49f98b4a1
Bhuttu03@ <sambit2003sarkar@gmail.com>
2022-10-02 08:46:51 +0530

    corrected comments

--- Dart/BubbleSort.dart
+++ Dart/BubbleSort.dart
@@ -1,25 +1,32 @@
-void BubbleSort(List list) { //Function to do the sorting, where the list is passed as an argument
-  if(list.length == 0) return;//Checks if the list has elements or not. If no element present, it exits the function
+//Dart program to sort a list using bubble sort
+
+
+//Method to sort the list, where the list is passed as an argument
+void BubbleSort(List list) {     //Function to do the sorting, where the list is passed as an argument
+  if(list.length == 0) return;   //Checks if the list has elements or not. If no element present, it exits the function
 
   int n = list.length;
   int i, j;
   for(i=0;i<n;i++) {
-    for(j=0;j<n-i-1; j++) {//last i elements are already sorted. -1 is done as indexing of list starts from 0
-      if(list[j]>list[j+1]) {//Switch '>' to '<' to sort the list in descending order
+    for(j=0;j<n-i-1; j++) {     //last i elements are already sorted. -1 is done as indexing of list starts from 0
+      if(list[j]>list[j+1]) {   //Switch '>' to '<' to sort the list in descending order
         swap(list, j);
       }
     }
   }
 }
 
-void swap(List list, int j) {//method to swap jth and j+1th element of the array using a third variable
+//Utility function
+//Method to swap jth and j+1th element of the array using a third variable
+void swap(List list, int j) {
   int temp = list[j];
   list[j]=list[j+1];
   list[j+1]=temp;
 }
 
-void main() {//main method to test the code. The coderunner will start from here.
-  List list = [3,7,1,9,4,6,8];//a list can be taken as an input also.
+//Driver code
+void main() {
+  List list = [3,7,1,9,4,6,8];  //a list can be taken as an input also.
   BubbleSort(list);  
   print("Sorted list is: $list");
 }
