d350d5aae83abcd3fd1407d9ddfffd3b6fe106bf
Affan Ali <affanali577@gmail.com>
2022-10-07 10:36:53 +0500

    Quick Sorting in PHP

---
+++ PHP/Programs/quick_sort.php
@@ -0,0 +1,36 @@
+<?php
+//Quick Sorting in PHP
+
+function quick_sort($array)
+ {
+	$lower = $great = array();
+	if(count($array) < 2)
+	{
+		return $array;
+	}
+	$pivot_key = key($array);
+	$pivot = array_shift($array);
+	foreach($array as $value)
+	{
+		if($value <= $pivot)
+		{
+			$lower[] = $value;
+		}elseif ($value > $pivot)
+		{
+			$great[] = $value;
+		}
+	}
+    var_dump($lower , $great);die;
+	return array_merge(quick_sort($lower),array($pivot_key=>$pivot),quick_sort($great));
+}
+ 
+$array = array(-5, 3, 0, 7, 5, -1, 4, 1 , 9, 2);
+echo '<br />';
+echo 'Input Array : '.implode(',',$array).'\n';
+echo '<br />';
+$array = quick_sort($array);
+echo '<br />';
+echo 'After Sorting : '.implode(',',$array);
+
+
+?>
