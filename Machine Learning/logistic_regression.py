662f0113a1c099b763ae3b7949bfe178d58ba956
Tahseen Ahmed <93079656+TahseenAhmed29@users.noreply.github.com>
2022-10-11 00:09:18 +0500

    Create logistic_regression.py

---
+++ Machine Learning/logistic_regression.py
@@ -0,0 +1,13 @@
+import numpy
+from sklearn import linear_model
+
+#Reshaped for Logistic function.
+X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+logr = linear_model.LogisticRegression()
+logr.fit(X,y)
+
+#predict if tumor is cancerous where the size is 3.46mm:
+predicted = logr.predict(numpy.array([3.46]).reshape(-1,1))
+print(predicted)
