1b1ad7d703c6ee5ed7eb2f76c06c9960c32ea84d
devonhunt <113886619+devonhunt@users.noreply.github.com>
2022-10-17 11:45:48 +0500

    Create helloworld.dart

---
+++ Dart/helloworld.dart
@@ -0,0 +1,11 @@
+void main() {
+  var String = "MLSA MUET SZAB";
+  var String1 = "mlsa muet szab";
+  print(String.toLowerCase());
+  print(String.toUpperCase());
+  print(String.trim());
+  print(String.length);
+  print(String.isNotEmpty);
+  print(String.compareTo(String1));
+  print('Hello World');
+}
