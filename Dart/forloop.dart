0083a006ddf51e883fcbceb32484d2ac9a896966
devonhunt <113886619+devonhunt@users.noreply.github.com>
2022-10-17 11:45:47 +0500

    Create forloop.dart

---
+++ Dart/forloop.dart
@@ -0,0 +1,15 @@
+void main () {
+  // Print numbers from 1 to 10
+  for(var i=1; i<=10; i++){
+    print(i);
+  }
+  
+  print("");
+  
+  // Print even numbers from 0 to 10
+  for(var j=0; j<=10; j++){
+    if(j%2==0){
+      print(j);
+    }
+  }
+}
