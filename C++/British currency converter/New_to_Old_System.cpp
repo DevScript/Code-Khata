80d9b7dae55611b3921b4ecc0c50698d0f96e35a
YusraSaleem <107356406+YusraSaleem@users.noreply.github.com>
2022-10-19 01:20:20 +0500

    Currency converter
    This converter will convert currency from old monetary system(pounds, shillings, pence) of British to new one (decimal system) and vice versa.

---
+++ C++/British currency converter/New_to_Old_System.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+	
+	float decpounds;
+	cout<<"Enter decimal pounds: ";
+	cin>>decpounds;     
+	
+	float x,y,z,x1,x2,y1,y2;
+	x=(int) decpounds;   //pounds
+	x1=decpounds-x;
+	y1= (x1*20);    // shelling
+	y= (int) (x1*20);
+	y2= y1-y; 
+	z=(int) (y2*12);//pence 
+	
+	cout<<"Equivalent in old notation = "<<'\x9c'<<x<<"."<<y<<"."<<z<<".";
+	return 0;
+}
