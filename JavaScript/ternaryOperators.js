1f85c9b3ff04334c6599fb7b695cd687c8cd591c
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2022-10-18 08:10:06 +0500

    Create ternaryOperators.js

---
+++ JavaScript/ternaryOperators.js
@@ -0,0 +1,23 @@
+// Ternary Operator in Javascript
+
+// Ternary operators are conditional operators and takes three operands.
+// condition to check with ?
+// an expression to execute if the condition is true with :
+// and last expression if the given condition is false
+
+let a = 4;
+let b = 3;
+
+a == b ? console.log("a and b are equal") : console.log("a and b are not equal");
+// OUTPUT
+// a and b are not equal
+
+
+// For Multiple Conditions 
+a > b
+  ? console.log("a is greater")
+  : a < b
+  ? console.log("b is greater")
+  : console.log(" a and b are equal");
+// OUTPUT
+// a is greater
