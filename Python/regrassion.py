0418250b30da157ff9d370d8ca9b6be51c290c4f
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2022-10-18 08:19:08 +0500

    Create regrassion.py

---
+++ Python/regrassion.py
@@ -0,0 +1,12 @@
+import re
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print ("matchObj.group() : ", matchObj.group())
+   print ("matchObj.group(1) : ", matchObj.group(1))
+   print ("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print ("No match!!")
