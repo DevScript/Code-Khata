9cf039d9683d065a1db0f7c1579e441b5a9a145f
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2022-10-18 08:36:12 +0500

    Create sudoku_solver.py

---
+++ Python/sudoku_solver.py
@@ -0,0 +1,63 @@
+"""
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution.
+
+"""
+
+class Solution:
+    def solve(self, board):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    for k in range(1, 10):
+                        if self.isValid(board, i, j, str(k)):
+                            board[i][j] = str(k)
+                            if self.solve(board):
+                                return True
+                            else:
+                                board[i][j] = '.'
+                    return False
+        return True
+
+
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        self.solve(board)
+
+    def isValid(self, board, i, j, k):
+        for m in range(9):
+            if board[m][j] == k:
+                return False
+            if board[i][m] == k:
+                return False
+            if board[3*(i//3)+m//3][3*(j//3)+m%3] == k:
+                return False
+        return True
+        
