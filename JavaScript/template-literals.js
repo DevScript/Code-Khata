395211c8bc5ac61d65177534c06a74570c5682e8
Mursal Furqan <36442744+mursalfk@users.noreply.github.com>
2022-10-18 08:10:03 +0500

    Create template-literals.js

---
+++ JavaScript/template-literals.js
@@ -0,0 +1,12 @@
+// JavaScript Template Literals 
+
+// Template Literals are enclosed with backticks ``
+// Allow variables and expression, use single and dual quotes, multiline in strings
+const years = 5
+console.log(`She has ${years} of experience in "Javascript" 
+She has worked for Multiple companies`)
+
+// OUTPUT
+
+// She has 5 of experience in "Javascript" 
+// She has worked for Multiple companies
