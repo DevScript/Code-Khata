76e151d5ab0f47067162906dbbf4f1d7734bbf37
mbutt20 <116273634+mbutt20@users.noreply.github.com>
2022-10-20 16:54:41 +0500

    Add files via upload

---
+++ key delete (1).py
@@ -0,0 +1,19 @@
+
+def phone_directory(a):
+    c=a.pop('daniyal')
+    print(a,'name daniyal is deleted')
+    c=0
+    for i in a.keys():
+        c+=1
+    print('')
+    print(c,'is now the total memeber in phone directory')
+#drivers code:
+a={'father':'0987','mother':'5678','me':'4567','daniyal':'3445','mus':'1233','myra':'098','samad':'534','mid':'234','maryam':'534','sani':'645','husnain':'765','rasif':'234'}
+phone_directory(a)
+
+
+# In[ ]:
+
+
+
+
